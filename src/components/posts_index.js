7edfb1a56bfd808f76bcbbb2899e9bb6fa0fe118
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-03-07 18:57:53 -0500

    Add link on posts_index page to posts_new page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 export default () => {
   return <div>List of blog posts</div>
@@ -13,7 +14,13 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+      </div>
     );
   }
 }
