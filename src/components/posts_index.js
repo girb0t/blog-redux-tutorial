c22832dfbbee77234652a41a615e7e3388b96cd8
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-03-06 23:48:43 -0500

    refactor PostsIndex to be a class component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 export default () => {
   return <div>List of blog posts</div>
 }
+
+class PostsIndex extends Component {
+  componentWillMount() {
+    console.log('fetch posts now!')
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+export default PostsIndex;
