018e64994ac14dea4500c6c9668ca99832e6a520
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-03-16 21:41:06 -0400

    Navigate back to index page on new post submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  onSubmit(formProps) { //formProps include title, categories, content
+    this.props.createPost(formProps)
+      .then(() => {
+        // blog post has been created, navigate the user to the index
+        // We navigate by calling this.context.router.push with the
+        // new path to navigate to.
+        this.context.router.push('/');
+      })
+  }
+
   render() {
     // this.props.handleSubmit is provided by the `reduxForm` function exported below (similar to the `connect` function)
     const { fields: { title, categories, content }, handleSubmit } = this.props;
@@ -11,7 +25,7 @@ class PostsNew extends Component {
     //same as `const title = this.props.fields.title`
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <h3>Create a New Post</h3>
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
