b704bfda96a5d8f79c1c7bc4a78ab46d9366ad27
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-03-17 13:01:26 -0400

    Create delete button on PostsShow page

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
 export const CREATE_POST = 'CREATE_POST';
+export const DELETE_POST = "DELETE_POST";
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=kjaksdlfjasdgljasdg';
@@ -17,7 +18,7 @@ export function fetchPosts() {
 }
 
 export function fetchPost(postId) {
-  const request = axios.get(`${ROOT_URL}/posts/${postId}${API_KEY}`)
+  const request = axios.get(`${ROOT_URL}/posts/${postId}${API_KEY}`);
 
   return {
     type: FETCH_POST,
@@ -30,6 +31,16 @@ export function createPost(props) {
 
   return {
     type: CREATE_POST,
+    payload: request
+  }
+}
+
+export function deletePost(postId) {
+  // api call returns post
+  const request = axios.delete(`${ROOT_URL}/posts/${postId}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request
   }
 }
